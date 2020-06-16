dca88e3cbc589408b6d1df19cb3bcc3933a357c4
amrudha_webnexs <amrudha@webnexs.com>
2020-06-16 12:51:51 +0530

    article controller changes

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -20,21 +20,21 @@ public function __construct()
 	}
 
 
-	public function detail_page($artile_id='')
+	public function detail_page($article_id='')
 	{
-		if($artile_id!=''){
+		if($article_id!=''){
 			$this->db->select('*');
 			$this->db->from('article');
-			$this->db->join('featuredsicknesses', 'featuredsicknesses.article_idarticle = article.idarticle');
-			$this->db->where('article.idarticle',$artile_id);
+			$this->db->join('featuredsicknesses', 'featuredsicknesses.article_idarticle = article.idarticle','LEFT');
+			$this->db->where('article.idarticle',$article_id);
 			$data['article_details']= $this->db->get()->result_array();
-  			$get_related_sickeness = $this->db->query("select sickness_idsickness from featuredsicknesses where article_idarticle='$artile_id'")->result_array();
+  			$get_related_sickeness = $this->db->query("select sickness_idsickness from featuredsicknesses where article_idarticle='$article_id'")->result_array();
 			if(count($get_related_sickeness) > 0){
 				$this->db->select('*');
 				$this->db->from('article');
 				$this->db->join('featuredsicknesses', 'featuredsicknesses.article_idarticle = article.idarticle');
 				$this->db->where('featuredsicknesses.sickness_idsickness',$get_related_sickeness[0]['sickness_idsickness']);
-				$this->db->where('article.idarticle!=',$artile_id);
+				$this->db->where('article.idarticle!=',$article_id);
 				$this->db->limit(3,0);
 				$this->db->order_by("created_at", "desc");
 				$data['get_related_article'] = $this->db->get()->result_array();
