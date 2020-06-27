07e5dbbb45f4950ba3a3e7b0afdb6539e9cbdab6
sakthi keerthiga <shakthi.s@henote.net>
2020-06-27 19:24:42 +0530

    testimony and grocery crud

--- application/views/add_testimony.php
+++ application/views/add_testimony.php
@@ -35,106 +35,106 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           </p>
 
         </div>
- <form action="#" method="POST">
+        <form action="<?php echo base_url();?>save-testimony" method="POST">
 
-            <div class="form-group row">
-              <label class="col-lg-2 col-md-3">Sickness idsickness</label>
-              <div class="col-md-8 xdr-select">
-                <select class="selectpicker" name="sickness_idsickness" data-live-search="true">
-                  <option selected="selected">Select Sickness idsickness</option>
-                  <?php foreach ($sickness as $key => $sick) { echo"<pre>";print_r($sick);?>
-                  <option value="<?php echo $sick['idsickness']?>"><?php echo $sick['commonName']?></option>
-                  <?php } ?>
-                </select>
-              </div>
-            </div>
+          <div class="form-group row">
+            <label class="col-lg-2 col-md-3">Sickness idsickness</label>
+            <div class="col-md-8 xdr-select">
+              <select class="selectpicker" name="sickness_idsickness" data-live-search="true" required>
+                <option selected="selected">Select Sickness idsickness</option>
+                <?php foreach ($sickness as $key => $sick) { echo"<pre>";print_r($sick);?>
+                <option value="<?php echo $sick['idsickness']?>"><?php echo $sick['commonName']?></option>
+              <?php } ?>
+            </select>
+          </div>
+        </div>
 
-            <div class="form-group row">
-              <label class="col-lg-2 col-md-3"> Remedy idremedy: </label>
-              <div class="col-md-8 xdr-select">
-                <select class="selectpicker" name="remedy_idremedy" data-live-search="true">
-                  <option selected="selected"> Select Remedy idremedy </option>
-                  <?php foreach ($remedies as $key => $remedy) { ?>
-                  <option value="<?php echo $remedy['idremedy'] ?>"><?php echo $remedy['name']?></option>
-                  <?php } ?>
-                </select>
-              </div>
-            </div>
+        <div class="form-group row">
+          <label class="col-lg-2 col-md-3"> Remedy idremedy: </label>
+          <div class="col-md-8 xdr-select">
+            <select class="selectpicker" name="remedy_idremedy" data-live-search="true" required>
+              <option selected="selected"> Select Remedy idremedy </option>
+              <?php foreach ($remedies as $key => $remedy) { ?>
+                <option value="<?php echo $remedy['idremedy'] ?>"><?php echo $remedy['name']?></option>
+              <?php } ?>
+            </select>
+          </div>
+        </div>
 
-            <div class="form-group row">
-              <label class="col-lg-2 col-md-3"> Administered By </label>
-              <div class="col-md-8 xdr-select">
-                <select class="selectpicker" name="administeredBy" data-live-search="true">
-                  <option selected="selected"> Select Administered By </option>
-                  <option value="1">Self</option>
-                  <option value="2">Medical Doctor</option>
-                  <option value="3">Other</option>
-                </select>
-              </div>
-            </div>
+        <div class="form-group row">
+          <label class="col-lg-2 col-md-3"> Administered By </label>
+          <div class="col-md-8 xdr-select">
+            <select class="selectpicker" name="administeredBy" data-live-search="true" required>
+              <option selected="selected"> Select Administered By </option>
+              <option value="1">Self</option>
+              <option value="2">Medical Doctor</option>
+              <option value="3">Other</option>
+            </select>
+          </div>
+        </div>
 
-            <div class="form-group row">
-              <label class="col-lg-2 col-md-3"> Compose </label>
-              <div class="col-md-8 xdr-select">
-                <textarea id="compose" class="form-control" name="story" rows="7" placeholder="Text formating here"></textarea>
-              </div>
-            </div>
+        <div class="form-group row">
+          <label class="col-lg-2 col-md-3"> Compose </label>
+          <div class="col-md-8 xdr-select">
+            <textarea id="compose" class="form-control" name="story" rows="7" placeholder="Text formating here"></textarea>
+          </div>
+        </div>
 
-            <div class="form-group row">
-              <label class="col-lg-2 col-md-3"> Dosage </label>
-              <div class="col-md-8 xdr-select">
-                <input type="text" class="form-control" name="dosage">
-              </div>
-            </div>
+        <div class="form-group row">
+          <label class="col-lg-2 col-md-3"> Dosage </label>
+          <div class="col-md-8 xdr-select">
+            <input type="text" class="form-control" name="dosage" required>
+          </div>
+        </div>
 
-            <div class="form-group row">
-              <label class="col-lg-2 col-md-3"> Relief Id Relief </label>
-              <div class="col-md-8 xdr-select">
-                  <select class="selectpicker" name="relief_idrelief" data-live-search="true">
-                  <option selected="selected"> Select Relief Id Relief</option>
-                  <?php foreach ($relief_type as $key => $relief) { ?>
-                  <option value="<?php echo $relief['idrelief'] ?>"><?php echo $relief['type']?></option>
-                  <?php } ?>
-                </select>
-              </div>
-            </div>
+        <div class="form-group row">
+          <label class="col-lg-2 col-md-3"> Relief Id Relief </label>
+          <div class="col-md-8 xdr-select">
+            <select class="selectpicker" name="relief_idrelief" data-live-search="true" required>
+              <option selected="selected"> Select Relief Id Relief</option>
+              <?php foreach ($relief_type as $key => $relief) { ?>
+                <option value="<?php echo $relief['idrelief'] ?>"><?php echo $relief['type']?></option>
+              <?php } ?>
+            </select>
+          </div>
+        </div>
 
-            <div class="form-group row">
-              <label class="col-lg-2 col-md-3"> Administered To </label>
-              <div class="col-md-8 xdr-select">
-                <select class="selectpicker" name="administeredTo" data-live-search="true">
-                  <option selected="selected"> Select Administered To </option>
-                  <option value="1">Self</option>
-                  <option value="2">Patient</option>
-                  <option value="3">Other</option>
-                </select>
-              </div>
-            </div>
+        <div class="form-group row">
+          <label class="col-lg-2 col-md-3"> Administered To </label>
+          <div class="col-md-8 xdr-select">
+            <select class="selectpicker" name="administeredTo" data-live-search="true" required>
+              <option selected="selected"> Select Administered To </option>
+              <option value="1">Self</option>
+              <option value="2">Patient</option>
+              <option value="3">Other</option>
+            </select>
+          </div>
+        </div>
 
-            <div class="form-group row">
-              <label class="col-lg-2 col-md-3"> Text formating here </label>
-              <div class="col-md-8 xdr-select">
-                <textarea id="textformat" class="form-control" name="warnings" rows="3" placeholder="Text formating here"></textarea>
-              </div>
-            </div>
+        <div class="form-group row">
+          <label class="col-lg-2 col-md-3"> Text formating here </label>
+          <div class="col-md-8 xdr-select">
+            <textarea id="textformat" class="form-control" name="warnings" rows="3" placeholder="Text formating here"></textarea>
+          </div>
+        </div>
 
-            <div class="row mt-5">
-              <div class="col-12 text-center">
-                <button class="btn btn-primary px-5">SUBMIT</button>
-              </div>
-            </div>
-          </form>
+        <div class="row mt-5">
+          <div class="col-12 text-center">
+            <button class="btn btn-primary px-5">SUBMIT</button>
+          </div>
+        </div>
+      </form>
 
-    
 
-      </div> <!-- END col-12 -->
 
-    </div> <!-- END row -->
+    </div> <!-- END col-12 -->
 
-    <div class="row">
+  </div> <!-- END row -->
 
-    </div><!-- END row -->
-  </div>
+  <div class="row">
+
+  </div><!-- END row -->
+</div>
 </section>
 <!-- section End -->
 
